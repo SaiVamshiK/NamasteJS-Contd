cef48a8238c0845e9384d9a217b714eae949af0f
SaiVamshiK <saivamshi.k.24@gmail.com>
2023-03-25 20:02:45 +0530

    Lesson 8 : converting function component to class component

--- src/components/About.js
+++ src/components/About.js
@@ -1,16 +1,20 @@
 import React from "react";
-import Profile from './Profile';
+import Profile from "./Profile";
 import ProfileClass from "./ProfileClass";
-
-const About = () => {
+export class About extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
     return (
-        <React.Fragment>
-            <h1>About Us Page</h1>
-            <p>This is the Namaste React live course</p>
-            <Profile name={"Vamshi"}/>
-            <ProfileClass name={"Vamshi Class"}/>
-        </React.Fragment>
+      <React.Fragment>
+        <h1>About Us Page</h1>
+        <p>This is the Namaste React live course</p>
+        <Profile name={"Vamshi"} />
+        <ProfileClass name={"Vamshi Class"} />
+      </React.Fragment>
     );
-};
+  }
+}
 
 export default About;
