de6e37ea2d8ed2ae36d32d1b5b1423acdf6215a5
SaiVamshiK <saivamshi.k.24@gmail.com>
2023-03-25 19:15:11 +0530

    Lesson 8 : creating state for both function and class

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,8 +1,14 @@
+import { useState } from "react";
+
 const Profile = (props) => {
-    console.log(props.name)
+
+    let [count,setCount] = useState(0);
+
+    console.log(props.name);
     return (
         <div>
             <h2>Profile Component</h2>
+            <h2>Count: {count}</h2>
         </div>
     );
 }
--- src/components/ProfileClass.js
+++ src/components/ProfileClass.js
@@ -1,11 +1,22 @@
 import React from "react";
 
 class ProfileClass extends React.Component {
+    constructor(props){
+        super(props);
+        // constructor is the place used for initialization.
+        // here the state variables are created.
+        this.state = {
+            count1 : 0,
+            count2 : 0
+        };
+    }
     render() {
         return (
             <>
                 <h1>Profile Class Component</h1>
                 <h2>Name : {this.props.name}</h2>
+                <h2>Count1: {this.state.count1}</h2>
+                <h2>Count2: {this.state.count2}</h2>
             </>
         );
     }
