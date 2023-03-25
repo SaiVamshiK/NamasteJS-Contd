c9c9bb0f59de752a88781ae81fb3e33d216f0292
SaiVamshiK <saivamshi.k.24@gmail.com>
2023-03-25 12:40:05 +0530

    Lesson 7 : logging error with Routers

--- src/components/Error.js
+++ src/components/Error.js
@@ -1,4 +1,10 @@
+import { useRouteError } from "react-router";
+
+// useRouteError is a hook;
+// This hook provides more details about the errors so that we can use it in displaying a meaningful error page.
 const Error = () => {
+    const error = useRouteError();
+    console.log(error);
     return (
         <div>
             <h1>Oops!!</h1>  
