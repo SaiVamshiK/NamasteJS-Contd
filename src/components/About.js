b15b69bb4aaf22d5bd18d260cf24a6caf6576b63
SaiVamshiK <saivamshi.k.24@gmail.com>
2023-03-25 20:11:41 +0530

    Lesson 8 : order of execution of statements (lifecycle methods)

--- src/components/About.js
+++ src/components/About.js
@@ -4,17 +4,30 @@ import ProfileClass from "./ProfileClass";
 export class About extends React.Component {
   constructor(props) {
     super(props);
+    console.log('About : Constructor called');
+  }
+  componentDidMount() {
+    // Best place to make an API call
+    console.log('About : Component Did Mount called');
   }
   render() {
+    console.log('About : Render called');
     return (
       <React.Fragment>
         <h1>About Us Page</h1>
         <p>This is the Namaste React live course</p>
-        <Profile name={"Vamshi"} />
         <ProfileClass name={"Vamshi Class"} />
       </React.Fragment>
     );
   }
 }
+
+// Sequence of steps
+// About.js:7 About : Constructor called - About Component lifecycle
+// About.js:14 About : Render called - About Component lifecycle
+// ProfileClass.js:12 Class Constructor called - Child Profile Component lifecycle
+// ProfileClass.js:23 Class Render called - Child Profile Component lifecycle
+// ProfileClass.js:17 Component Did Mount - Child Profile Component lifecycle ended
+// About.js:11 About : Component Did Mount called - About Component lifecycle ended
 
 export default About;
