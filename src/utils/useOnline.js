ced30c733d9b37be7989149a219df9bc89403bd0
SaiVamshiK <saivamshi.k.24@gmail.com>
2023-03-27 10:10:59 +0530

    Lesson 9 : internet connection check better code

--- src/utils/useOnline.js
+++ src/utils/useOnline.js
@@ -3,12 +3,18 @@ import {  useEffect, useState } from "react";
 const useOnline = () => {
   let [isOnline, setIsOnline] = useState(true);
   useEffect(() => {
-    window.addEventListener("online", () => {
-      setIsOnline(true);
-    });
-    window.addEventListener("offline", () => {
-      setIsOnline(false);
-    });
+    const handleOnline = () => {
+        setIsOnline(true);
+    }
+    const handleOffline = () => {
+        setIsOnline(false);
+    }
+    window.addEventListener("online",handleOnline);
+    window.addEventListener("offline",handleOffline);
+    return () => {
+        window.removeEventListener('online',handleOnline);
+        window.removeEventListener('offline',handleOffline);
+    }
   }, []);
   console.log('Use Online hook rendered');
   return isOnline;
