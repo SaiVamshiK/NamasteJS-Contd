8ed1881efd611e8cfe74b0f79c6c7f34b391a105
SaiVamshiK <saivamshi.k.24@gmail.com>
2023-04-03 11:05:20 +0530

    Lesson 12 : created Store

---
+++ src/utils/Store.js
@@ -0,0 +1,8 @@
+import { configureStore } from "@reduxjs/toolkit";
+
+// configureStore comes from the core redux library.
+const Store = configureStore({
+
+});
+// The store will have slices
+export default Store;
