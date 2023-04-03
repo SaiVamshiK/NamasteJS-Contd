85a1673238274d28b62f9a0904703e0512a1ce5b
SaiVamshiK <saivamshi.k.24@gmail.com>
2023-04-03 11:25:58 +0530

    Lesson 12 : actions added to the Redux Slice

--- src/utils/CartSlice.js
+++ src/utils/CartSlice.js
@@ -13,7 +13,14 @@ const CartSlice = createSlice({
         // state is the initial state and action is the data that is coming in.
         addItem: (state,action) => {
             state.items.push(action.payload);
+        },
+        removeItem: (state,action) => {
+            state.items = state.items.filter((item) => item !== action.payload)
+        },
+        clearCart: (state) => {
+            state.items = []
         }
+        // we do not have action in clearCart since when we dispatch the clearCart action then we do not send any data
     }
 });
 // Reducer functions are used to modify the cart slice
