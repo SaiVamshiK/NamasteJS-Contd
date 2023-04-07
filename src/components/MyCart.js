af0d5f7bb6ae3d81197fb9c63ad0dfafa5921600
SaiVamshiK <saivamshi.k.24@gmail.com>
2023-04-07 18:25:29 +0530

    Lesson 12 : better way to render cart page

--- src/components/MyCart.js
+++ src/components/MyCart.js
@@ -1,7 +1,7 @@
 import { useSelector } from "react-redux";
 
 const MyCart = () => {
-    const cartItems = useSelector((store) => store);
+    const cartItems = useSelector((store) => store.cart.items);
     // useSelector is used to subscribe to the store.
     // This is the place we tell what we are subscribing to.
     // If we write store => store : it subscribes to the entire store.
@@ -9,8 +9,11 @@ const MyCart = () => {
     console.log(cartItems);
     return (
         <div>
-            <h1 className="font-bold p-5 m-5">Cart Items - {cartItems.cart.items.length}</h1>
+            <h1 className="font-bold p-5 m-5">Cart Items - {cartItems.length}</h1>
         </div>
     );
 }
 export default MyCart;
+// This is a major performance issue.
+// If we subscribe to the entire store, each time any slice changes this component is re rendered again and again.
+// So instead it is better to subscribe to only a slice or even better just a particular variable of a slice.
