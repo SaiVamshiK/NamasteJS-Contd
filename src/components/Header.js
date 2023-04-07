763d628bc114ecc57ebcf476fe08b58957039447
SaiVamshiK <saivamshi.k.24@gmail.com>
2023-04-07 16:44:03 +0530

    Lesson 12 : subscribing to the cart slice items

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,10 +2,14 @@ import { useState,useContext } from "react";
 import { Title } from "./Title";
 import { Link } from "react-router-dom";
 import UserContext from "../utils/UserContext";
+import { useSelector } from "react-redux";
 
 const Header = () => {
   let [isLoggedIn, setIsLoggedIn] = useState(false);
   const {user} = useContext(UserContext);
+  const cartItems = useSelector(store => store.cart.items);
+  // subscribing to the cart items
+  // We have just subscribed to the Cart Slice's items.
   return (
     <div className="flex justify-between bg-pink-50 shadow-lg">
       <Title />
@@ -21,10 +25,10 @@ const Header = () => {
             <Link to="/contact">Contact</Link>
           </li>
           <li className="px-2">
-            <Link to='/cart'>Cart</Link>
+            <Link to='/cart'>Cart - 4 items</Link>
           </li>
-          <h2 className="font-bold p-2 m-2">{user.name}</h2>
-          {isLoggedIn ? (
+          {/* <h2 className="font-bold p-2 m-2">{user.name}</h2> */}
+          {/* {isLoggedIn ? (
             <>
               <button
                 onClick={() => {
@@ -44,7 +48,7 @@ const Header = () => {
                 Login
               </button>
             </>
-          )}
+          )} */}
         </ul>
       </div>
     </div>
